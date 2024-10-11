54aaab23c3ed8432e548ec9334c29f1346ed7d24
FariassArthur <p.fariasarthur@protonmail.com>
2024-10-10 21:00:32 -0300

    fix

---
+++ front/src/components/Aside/components/search.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//css
+import styled from 'styled-components'
+
+type Props = {}
+
+const search = (props: Props) => {
+  return (
+    <input type="search" />
+  )
+}
+
+export default search
