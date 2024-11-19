1e7cba8d5a2b925feb791039be97a80885d75fc0
FariassArthur <p.fariasarthur@protonmail.com>
2024-11-18 22:37:30 -0300

    testing favorite component

---
+++ front/src/components/Aside/components/AsideList.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+type Props = {}
+
+const AsideList = (props: Props) => {
+  return (
+    <Main>
+      <ul>
+        <li>teste</li>
+        <li>teste</li>
+      </ul>
+    </Main>
+  )
+}
+
+export default AsideList
+
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+
+
+`
